a954039a74f146a77172d3678b5158da5669bf45
sh1ckdev <makerofthefuture@gmail.com>
2026-02-27 18:05:48 +0300

    initial

---
+++ init-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+DB_NAME="${DB_NAME:-hackathon_db}"
+DB_USER="${DB_USER:-postgres}"
+
+echo "Проверка существования базы данных $DB_NAME..."
+
+# Проверяем, существует ли БД
+if psql -U "$DB_USER" -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
+    echo "База данных $DB_NAME уже существует"
+else
+    echo "Создание базы данных $DB_NAME..."
+    psql -v ON_ERROR_STOP=1 -U "$DB_USER" <<-EOSQL
+        CREATE DATABASE $DB_NAME;
+EOSQL
+    echo "База данных $DB_NAME создана успешно"
+fi
